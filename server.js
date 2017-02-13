be95b859c19d548812cdafad4230fd81fb1b5d9b
meghanroserebecca <meghanroserebecca@gmail.com>
2017-02-12 22:34:29 -0800

    coded along with book and got stuck: server has ceased to launch and 'handle' in index.js is undefined

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http');
+const url = require('url');
+const router = require('./router')
 
 
-function start() {
+function start(route, handle) {
     function onRequest(request, response) {
-        console.log("Request Received");
+        const pathname = url.parse(request.url).pathname;
+        console.log('Request for' + pathname + ' received');
+
+        route(handle, pathname);
+
         response.writeHead(200, {'Content-type': 'text/plain'});
         response.write('Hello World');
         response.end();
